6aa1e1077804acce619325630ee318de49feaf4d
cezary-pokropek <cezary.pokropek@gmail.com>
2021-12-16 13:20:41 +0100

    setting AWS Lambda

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Setting the handler, lambda function connection to this handler
+handler = Mangum(app=app)
